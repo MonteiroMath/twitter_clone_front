b3789e320a2b7cbbd6311f58459a74b44e70a55e
Matheus Antunes Monteiro <math.a.monteiro@gmail.com>
2021-05-06 10:00:45 -0300

    InteractionBar spacing

--- src/components/InteractionBar.js
+++ src/components/InteractionBar.js
@@ -2,16 +2,31 @@ import React from "react";
 import heart from "../icons/heart.svg";
 import retweet from "../icons/retweet.svg";
 import comment from "../icons/comment.svg";
+import share from "../icons/share.svg";
 
 function InteractionBar(props) {
   let { likes, retweets, comments } = props;
 
   return (
-    <div>
-      <img src={heart} alt="like icon" width="15px" />
-      {likes}
-      <img src={retweet} alt="like icon" width="15px" /> {retweets}
-      <img src={comment} alt="like icon" width="15px" /> {comments}
+    <div className="d-flex pt-2">
+      <div className="mr-5">
+        <img src={comment} alt="like icon" width="15px" />
+        <span> &nbsp;{comments}</span>
+      </div>
+
+      <div className="mr-5">
+        <img src={retweet} alt="like icon" width="15px" />
+        <span> &nbsp;{retweets}</span>
+      </div>
+
+      <div className="mr-5">
+        <img src={heart} alt="like icon" width="15px" />
+        <span> &nbsp;{likes}</span>
+      </div>
+
+      <div>
+        <img src={share} alt="share icon" width="15px" />
+      </div>
     </div>
   );
 }
