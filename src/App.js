0a22fd7f1be247416e58df1030022107d4e960e0
Matheus Antunes Monteiro <math.a.monteiro@gmail.com>
2021-05-06 10:43:37 -0300

    Avatar fixes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./App.css";
 import "./components/Feed";
 import Feed from "./components/Feed";
 
--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import avatar from "../images/phavatar.png";
+import avatar2 from "../images/avatar.png";
 
 function Avatar(props) {
+  let [src, setSrc] = useState(avatar2);
+
+  function fallbackSrc(e) {
+    setSrc(avatar);
+  }
+
   return (
-    <div className="pt-3">
-      <img src={avatar} alt="like icon" width="50px" />
+    <div>
+      <img
+        src={src}
+        onError={fallbackSrc}
+        alt="like icon"
+        width="50px"
+        height="50px"
+      />
     </div>
   );
 }
