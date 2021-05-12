8c7a39a46a556ae0f10d37c4b90385595b483656
Matheus Antunes Monteiro <math.a.monteiro@gmail.com>
2021-05-12 11:33:54 -0300

    added avatar and follow buttons to rightbar

--- src/components/TopicSection.js
+++ src/components/TopicSection.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { Button, Row } from "reactstrap";
 
 function TopicSection(props) {
   const { topic, genre } = props.content;
 
   return (
     <a href="#">
-      <div className="border-bottom p-2">
-        <div className="text-dark cfw-bolder">{topic}</div>
-        <div className="cfs-small text-black-50">{genre}</div>
-      </div>
+      <Row className="border-bottom p-2" noGutters>
+        <div>
+          <div className="text-dark cfw-bolder">{topic}</div>
+          <div className="cfs-small text-black-50">{genre}</div>
+        </div>
+        <div className="ml-auto">
+          <Button color="info" size="sm" outline rounded>
+            Follow
+          </Button>
+        </div>
+      </Row>
     </a>
   );
 }
--- src/components/WtF_Section.js
+++ src/components/WtF_Section.js
@@ -1,14 +1,27 @@
 import React from "react";
+import { Button, Row, Col } from "reactstrap";
+import phavatar from "../images/phavatar.png";
+import Avatar from "./Avatar";
 
 function WtF_Section(props) {
   const { name } = props.content;
 
   return (
     <a href="#">
-      <div className="border-bottom p-2">
-        <div className="text-dark cfw-bolder">{name}</div>
-        <div className="cfs-small text-black-50">{"@" + name}</div>
-      </div>
+      <Row className="border-bottom p-2" noGutters>
+        <Col lg="2">
+          <Avatar />
+        </Col>
+        <Col className="text-truncate ml-3" lg="6">
+          <div className="text-dark cfw-bolder">{name}</div>
+          <div className="cfs-small text-black-50">{"@" + name}</div>
+        </Col>
+        <div className="ml-auto">
+          <Button color="info" size="sm" outline rounded>
+            Follow
+          </Button>
+        </div>
+      </Row>
     </a>
   );
 }
