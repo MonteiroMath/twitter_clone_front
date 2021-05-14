14bb49b158c3db69a28e392efc7e04b5e54d9709
Matheus Antunes Monteiro <math.a.monteiro@gmail.com>
2021-05-14 09:35:48 -0300

    LeftNavBar positioning fixed

--- src/components/LeftBar.js
+++ src/components/LeftBar.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { Col } from "reactstrap";
+import { Row, Col, Button } from "reactstrap";
+
+import LeftNavBar from "./LeftNavBar";
 
 function LeftBar(props) {
   return (
-    <Col className="d-none d-md-block" md="2" lg="3">
-      Placeholder
+    <Col className="d-none d-md-block pt-1" md="2" lg="3">
+      <Row className="justify-content-end justify-content-lg-center" noGutters>
+        <LeftNavBar />
+      </Row>
+
+      <Row className="justify-content-center mt-5">profile info</Row>
     </Col>
   );
 }
---
+++ src/components/LeftNavBar.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import { Navbar, Nav, NavItem, NavLink, NavbarBrand, Button } from "reactstrap";
+
+import tweet from "../icons/tweet.svg";
+import home from "../icons/home.svg";
+import search from "../icons/search.svg";
+import bell from "../icons/bell.svg";
+import mail from "../icons/mail.svg";
+import feather from "../icons/feather.svg";
+
+function LeftNavBar(props) {
+  return (
+    <Navbar className="mr-3">
+      <Nav className="align-items-center align-items-lg-start" navbar>
+        <NavbarBrand className="mr-0">
+          <img src={tweet} alt="tweet icon" width="35px" />
+        </NavbarBrand>
+        <NavItem>
+          <NavLink href="#">
+            <img src={home} alt="home icon" width="25px" />
+            <span className="ml-3 d-none d-lg-inline">Home</span>
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">
+            <img src={search} alt="explore icon" width="25px" />
+            <span className="ml-3 d-none d-lg-inline">Explore</span>
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">
+            <img src={bell} alt="notifications icon" width="25px" />
+            <span className="ml-3 d-none d-lg-inline">Notifications</span>
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">
+            <img src={mail} alt="messages icon" width="25px" />
+            <span className="ml-3 d-none d-lg-inline">Messages</span>
+          </NavLink>
+        </NavItem>
+        <Button color="info" className="mt-4">
+          <div>
+            <img
+              className="d-lg-none"
+              src={feather}
+              alt="new tweet icon"
+              width="20px"
+            />
+            <span className="d-none d-lg-inline">Tweet</span>
+          </div>
+        </Button>
+      </Nav>
+    </Navbar>
+  );
+}
+
+export default LeftNavBar;
