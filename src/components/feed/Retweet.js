d591da7b7c1a81bf14963b9362576d0bf0db4908
Matheus Antunes Monteiro <math.a.monteiro@gmail.com>
2021-10-20 10:31:39 -0300

    aligned you retweeted text

--- src/components/feed/Retweet.js
+++ src/components/feed/Retweet.js
@@ -34,7 +34,11 @@ function Retweet(props) {
 
   return (
     <Row noGutters>
-      <Col className="ml-auto pb-1 cBold cfontSmall" xs={11}>
+      <Col
+        className="ml-auto pl-2 pl-md-3 pb-1 cBold cfontSmall"
+        xs="10"
+        md="11"
+      >
         <RetweetIcon />
         <span className="ml-2">You retweeted</span>
       </Col>
