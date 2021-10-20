66c7537a8653008d0837107760a8c2eb7edb400a
Matheus Antunes Monteiro <math.a.monteiro@gmail.com>
2021-10-20 10:44:13 -0300

    code cleanup on retweet.js

--- src/components/feed/Retweet.js
+++ src/components/feed/Retweet.js
@@ -1,16 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import { useDispatch } from "react-redux";
 import { Row, Col } from "reactstrap";
-
-import Attachment from "../Attachment";
-
-import Avatar from "../Avatar";
-import InfoBar from "./InfoBar";
-import Message from "./Message";
-import Poll from "./Poll";
-import InteractionBar from "./InteractionBar";
-import { ACTIONS } from "../../store/actions";
 
 import Tweet from "./Tweet";
 import { RetweetIcon } from "../svg/Svg";
@@ -18,19 +8,6 @@ import { RetweetIcon } from "../svg/Svg";
 function Retweet(props) {
   let { retweet, user } = props;
   let { tweet } = retweet;
-
-  const [liked, setLiked] = useState(false);
-  let dispatch = useDispatch();
-
-  function handleLike() {
-    let type = liked ? ACTIONS.UNLIKE : ACTIONS.LIKE;
-    dispatch({
-      type,
-      payload: { id: retweet.id },
-    });
-
-    setLiked(!liked);
-  }
 
   return (
     <Row noGutters>
