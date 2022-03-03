b4ab4b532bc7c1628a77efa495c4484a6b16a8b9
Matheus Antunes Monteiro <matheus@debian>
2022-03-03 11:16:10 -0300

    return button now pushes history to maintain state

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { Row, Col, Navbar, Nav, NavItem, NavLink } from "reactstrap";
 
 import { LeftArrow } from "./svg/Svg";
 
 export default function TopBar({ header }) {
+  let history = useHistory();
+
   return (
     <Row className="sticky-top border bg-white" noGutters>
       <Row className="w-100 px-2" noGutters>
@@ -11,7 +14,14 @@ export default function TopBar({ header }) {
           <Navbar>
             <Nav navbar>
               <NavItem>
-                <NavLink className="text-dark" href="/">
+                <NavLink
+                  className="text-dark"
+                  href="#"
+                  onClick={(ev) => {
+                    ev.preventDefault();
+                    history.push("/");
+                  }}
+                >
                   <LeftArrow />
                 </NavLink>
               </NavItem>
