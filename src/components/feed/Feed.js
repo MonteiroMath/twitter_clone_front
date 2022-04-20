021e3ba36467f75086bbdff6c2d89ad20157527c
Matheus <math.a.monteiro@gmail.com>
2022-04-20 09:08:39 -0300

    FeedTweetList, moving async tweetfetching logic to this component

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,41 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Row, Spinner } from "reactstrap";
+import { Row } from "reactstrap";
 
 import NewTweet from "../newTweet/NewTweet";
 
 import FeedNavbar from "./FeedNavbar";
-import TweetList from "./TweetList";
+import FeedTweetList from "./FeedTweetList";
 import NewTweetButton from "./NewTweetButton";
 import BottomBar from "./BottomBar";
 
-import { selectAllTweets, fetchTweets } from "../../store/tweetsSlice";
 import userData from "../../placeholders/user";
 
 function Feed(props) {
   let [user, setUser] = useState({});
-  const dispatch = useDispatch();
-
-  const tweetStatus = useSelector((state) => state.tweets.status);
-  const error = useSelector((state) => state.tweets.error);
-  const tweetList = [...useSelector(selectAllTweets)].reverse();
 
   useEffect(() => {
     setUser(userData);
-    if (tweetStatus === "idle") {
-      dispatch(fetchTweets(1));
-    }
   }, []);
-
-  let content;
-
-  if (tweetStatus === "fullfiled") {
-    content = <TweetList user={user} tweetList={tweetList} />;
-  } else if (tweetStatus === "pending") {
-    content = <Spinner color="info" />;
-  } else if (tweetStatus === "rejected") {
-    content = <div>{error}</div>;
-  }
 
   return (
     <div>
@@ -45,7 +25,7 @@ function Feed(props) {
       <Row className="border p-3 d-none d-md-flex" noGutters={true}>
         <NewTweet />
       </Row>
-      {content}
+      <FeedTweetList user={user} />
       <NewTweetButton />
       <BottomBar />
     </div>
---
+++ src/components/feed/FeedTweetList.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Spinner } from "reactstrap";
+import TweetList from "./TweetList";
+import { selectAllTweets, fetchTweets } from "../../store/tweetsSlice";
+
+function FeedTweetList({ user }) {
+  const dispatch = useDispatch();
+
+  const tweetStatus = useSelector((state) => state.tweets.status);
+  const error = useSelector((state) => state.tweets.error);
+  const tweetList = [...useSelector(selectAllTweets)].reverse();
+
+  useEffect(() => {
+    if (tweetStatus === "idle") {
+      dispatch(fetchTweets(1));
+    }
+  }, []);
+
+  let content = null;
+
+  if (tweetStatus === "fullfiled") {
+    content = <TweetList user={user} tweetList={tweetList} />;
+  } else if (tweetStatus === "pending") {
+    content = <Spinner color="info" />;
+  } else if (tweetStatus === "rejected") {
+    content = <div>{error}</div>;
+  }
+
+  return content;
+}
+
+export default FeedTweetList;
