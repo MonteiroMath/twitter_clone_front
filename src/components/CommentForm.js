ab26bd41e3a35895b6f640ace013e2c6fcbbc76a
Matheus <math.a.monteiro@gmail.com>
2022-06-27 09:00:01 -0300

    fixed toggle

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,10 +1,11 @@
 import React from "react";
 import NewTweetForm from "./newTweet/NewTweetForm";
 
-export default function CommentForm({ parent_id }) {
+export default function CommentForm({ parent_id, toggle }) {
   return (
     <NewTweetForm
       placeholder={"Answer this tweet"}
+      toggle={toggle}
       submit_text={"Comment"}
       parent_id={parent_id}
       noPoll={true}
