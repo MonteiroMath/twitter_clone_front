416609662caf64852d0972a80c03b6f405862185
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-03-25 11:47:35 -0300

    created the useTweetLoader hook

--- src/components/FeedPage/FeedPage.js
+++ src/components/FeedPage/FeedPage.js
@@ -4,6 +4,7 @@ import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import { Row } from "reactstrap";
 
 import { selectJwtToken, selectUserData } from "../../store/UserSlice";
+import useTweetLoader from "../../hooks/useTweetLoader";
 
 import MainLayout from "../MainLayout/MainLayout";
 import FeedNavbar from "./Navbar/FeedNavbar";
@@ -16,6 +17,9 @@ function Feed() {
   const jwtToken = useSelector((state) => selectJwtToken(state));
   const user = useSelector((state) => selectUserData(state));
 
+  //tweet State
+  const [tweetList, error, reqStatus] = useTweetLoader(user.username, jwtToken);
+
   return jwtToken ? (
     <MainLayout>
       <div>
@@ -25,7 +29,7 @@ function Feed() {
         <Row className="border p-3 d-none d-md-flex" noGutters={true}>
           <NewTweet />
         </Row>
-        <FeedTweetList username={user.username} />
+        <FeedTweetList tweetList={tweetList} error={error} status={reqStatus} />
         <NewTweetButton />
         <BottomBar />
       </div>
--- src/components/FeedPage/FeedTweetList/FeedTweetList.js
+++ src/components/FeedPage/FeedTweetList/FeedTweetList.js
@@ -1,42 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { selectJwtToken } from "../../../store/UserSlice";
+import React from "react";
+
 import { Spinner } from "reactstrap";
 import TweetList from "../../Shared/TweetList/TweetList";
-import { client } from "../../../api/client";
 
-function FeedTweetList({ username }) {
-  const jwtToken = useSelector((state) => selectJwtToken(state));
-
-  const [tweetList, setTweetList] = useState([]);
-  const [error, setError] = useState(null);
-  const [reqStatus, setReqStatus] = useState("idle");
-
-  useEffect(() => {
-    if (reqStatus === "idle") {
-      setReqStatus("loading");
-      client
-        .getTweetsByUsername(username, jwtToken)
-        .then((res) => {
-          if (res.success) {
-            setReqStatus("fulfilled");
-            setTweetList(res.tweets);
-          } else {
-            throw new Error(res.msg);
-          }
-        })
-        .catch((err) => {
-          setReqStatus("failed");
-          setError(err.message);
-        });
-    }
-  }, [reqStatus, username, jwtToken]);
-
+function FeedTweetList({ status, tweetList, error }) {
   return (
     <>
-      {reqStatus === "fulfilled" && <TweetList tweetList={tweetList} />}
-      {reqStatus === "loading" && <Spinner color="info" />}
-      {reqStatus === "failed" && <div>{error}</div>}
+      {status === "fulfilled" && <TweetList tweetList={tweetList} />}
+      {status === "loading" && <Spinner color="info" />}
+      {status === "failed" && <div>{error}</div>}
     </>
   );
 }
