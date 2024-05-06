8123d83ad020e224dc91cb8e0fc209f2057b4c6f
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-05-06 10:53:56 -0300

    changes to UserPage and UserProfile to improve testability

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,66 +1,18 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Spinner } from "reactstrap";
 
-import { client } from "../../api/client";
 
 import MainLayout from "../MainLayout/MainLayout";
 import UserProfile from "./UserProfile/UserProfile";
 import FeedTweetList from "../FeedPage/FeedTweetList/FeedTweetList";
-import { useSelector } from "react-redux";
-import { selectJwtToken } from "../../store/UserSlice";
+
 
 function UserPage() {
   let { username } = useParams();
 
-  const [loadingState, setLoadingState] = useState("idle");
-  const [user, setUser] = useState({});
-
-  const jwtToken = useSelector((state) => selectJwtToken(state));
-
-  useEffect(() => {
-    setLoadingState("loading");
-    client.getUserData(username, jwtToken).then((result) => {
-      if (result.success) {
-        setUser(result.user);
-        setLoadingState("success");
-      } else {
-        setLoadingState("failed");
-      }
-    });
-  }, [username, jwtToken]);
-
-  function handleFollow() {
-    const { isFollowed } = user;
-    const action = isFollowed ? client.unfollow : client.follow;
-
-    action(user.id, jwtToken).then((result) => {
-      if (result.success) {
-        setUser(result.user);
-      }
-    });
-  }
-
   return (
     <MainLayout>
-      {loadingState === "loading" && (
-        <div className="d-flex justify-content-center">
-          <Spinner color="info" />
-        </div>
-      )}
-
-      {loadingState === "success" && (
-        <>
-          <UserProfile user={user} handleFollow={handleFollow} />
-          <FeedTweetList username={username} />
-        </>
-      )}
-
-      {loadingState === "failed" && (
-        <div className="d-flex justify-content-center cfs-30 cfw-bold">
-          Something went wrong!
-        </div>
-      )}
+      <UserProfile username={username} />
+      <FeedTweetList username={username} />
     </MainLayout>
   );
 }
--- src/components/UserPage/UserProfile/UserProfile.js
+++ src/components/UserPage/UserProfile/UserProfile.js
@@ -1,77 +1,122 @@
-import placeholderBg from "../../../assets/images/bgplaceholder.gif";
-import Avatar from "../../Shared/Avatar/Avatar";
-import { Button } from "reactstrap";
-
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { Button, Spinner } from "reactstrap";
 import { IoIosLink } from "react-icons/io";
 import { IoCalendarOutline } from "react-icons/io5";
-import { useSelector } from "react-redux";
-import { selectUserData } from "../../../store/UserSlice";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
-function UserProfile({ user, handleFollow }) {
+import placeholderBg from "../../../assets/images/bgplaceholder.gif";
+import Avatar from "../../Shared/Avatar/Avatar";
+import { selectUserData, selectJwtToken } from "../../../store/UserSlice";
+import { client } from "../../../api/client";
+
+function UserProfile({ username }) {
+  const [loadingState, setLoadingState] = useState("idle");
+  const [user, setUser] = useState({});
+
   const localUser = useSelector((state) => selectUserData(state));
   const isLocalUserPage = localUser.id === user.id;
+  const jwtToken = useSelector((state) => selectJwtToken(state));
+
+  useEffect(() => {
+    setLoadingState("loading");
+    client.getUserData(username, jwtToken).then((result) => {
+      if (result.success) {
+        setUser(result.user);
+        setLoadingState("success");
+      } else {
+        setLoadingState("failed");
+      }
+    });
+  }, [username, jwtToken]);
+
+  function handleFollow() {
+    const { isFollowed } = user;
+    const action = isFollowed ? client.unfollow : client.follow;
+
+    action(user.id, jwtToken).then((result) => {
+      if (result.success) {
+        setUser(result.user);
+      }
+    });
+  }
 
   return (
-    <div className="profileContainer">
-      <img
-        src={user.background || placeholderBg}
-        alt="User background"
-        className="backgroundImg"
-      />
+    <>
+      {loadingState === "loading" && (
+        <div className="d-flex justify-content-center">
+          <Spinner color="info" />
+        </div>
+      )}
 
-      <div className="avatarAndEditProfileContainer">
-        <Avatar context="userProfile" avatar={user.avatar} />
+      {loadingState === "success" && (
+        <div className="profileContainer">
+          <img
+            src={user.background || placeholderBg}
+            alt="User background"
+            className="backgroundImg"
+          />
 
-        <div>
-          {isLocalUserPage ? (
-            <Button className="mt-2" color="primary" outline>
-              Edit Profile
-            </Button>
-          ) : (
-            <Button
-              className="mt-2"
-              color="primary"
-              outline
-              onClick={() => handleFollow()}
-            >
-              {user.isFollowed ? "Unfollow" : "Follow"}
-            </Button>
-          )}
+          <div className="avatarAndEditProfileContainer">
+            <Avatar context="userProfile" avatar={user.avatar} />
+
+            <div>
+              {isLocalUserPage ? (
+                <Button className="mt-2" color="primary" outline>
+                  Edit Profile
+                </Button>
+              ) : (
+                <Button
+                  className="mt-2"
+                  color="primary"
+                  outline
+                  onClick={() => handleFollow()}
+                >
+                  {user.isFollowed ? "Unfollow" : "Follow"}
+                </Button>
+              )}
+            </div>
+          </div>
+          <div className="profileInfo">
+            <div className="mb-3">
+              <div className="cfw-bolder cfs-20">{user.username}</div>
+              <div className="cfc-gray">{`@${user.username}`}</div>
+            </div>
+            <div className="mb-3">{user.description}</div>
+            <div className="mb-3">
+              <span>
+                {user.webpage ? (
+                  <a href="github.com">
+                    <IoIosLink /> {user.webpage}
+                  </a>
+                ) : null}
+              </span>{" "}
+              <span className="cfc-gray">
+                {" "}
+                <IoCalendarOutline /> {`Joined ${user.createdAt}`}
+              </span>
+            </div>
+            <div>
+              {" "}
+              <Link to={`/${user.username}/following`}>
+                <span className="cfw-bolder">{user.followedCount}</span>{" "}
+                <span className="cfc-gray">following</span> -{" "}
+              </Link>
+              <Link to={`/${user.username}/followers`}>
+                <span className="cfw-bolder">{user.followersCount}</span>{" "}
+                <span className="cfc-gray">followers</span>{" "}
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="profileInfo">
-        <div className="mb-3">
-          <div className="cfw-bolder cfs-20">{user.username}</div>
-          <div className="cfc-gray">{`@${user.username}`}</div>
+      )}
+
+      {loadingState === "failed" && (
+        <div className="d-flex justify-content-center cfs-30 cfw-bold">
+          Something went wrong!
         </div>
-        <div className="mb-3">{user.description}</div>
-        <div className="mb-3">
-          <span>
-            {user.webpage ? (
-              <a href="github.com">
-                <IoIosLink /> {user.webpage}
-              </a>
-            ) : null}
-          </span>{" "}
-          <span className="cfc-gray">
-            {" "}
-            <IoCalendarOutline /> {`Joined ${user.createdAt}`}
-          </span>
-        </div>
-        <div>
-          {" "}
-          <Link to={`/${user.username}/following`}>
-            <span className="cfw-bolder">{user.followedCount}</span>{" "}
-            <span className="cfc-gray">following</span> -{" "}
-          </Link>
-          <Link to={`/${user.username}/followers`}>
-            <span className="cfw-bolder">{user.followersCount}</span>{" "}
-            <span className="cfc-gray">followers</span>{" "}
-          </Link>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
